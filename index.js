f97853a7ee41cd512a3fd26504db9cc492d261df
SCP3812 Wojak <mrisaacjgraham@gmail.com>
2025-02-26 17:06:03 +0000

    first day changes

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs')
 
+var circle = {
+  x: 0,
+  y: 0, 
+  color:""
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -19,4 +25,12 @@ http.listen(3000, function() {
 // Handle a new connection
 io.on('connection', function(socket) {
 
+  socket.on("new client", function() {
+    console.log("a new client has connected")
+    io.emit("new circle")
+  })
+
+  socket.on("new circle", function(newCircle) {
+    io.emit('new circle', newCircle)
+  })
 })
--- public/script.js
+++ public/script.js
@@ -5,16 +5,39 @@ function setup() {
   canvas.parent('sketch-holder')
 
   background("lightblue")
+  clientPos = {
+    x: 0, 
+    y: 0,
+    color: "blue"
+  }
 }
+
+function draw() {
+  fill(clientPos.color)
+  noStroke();
+  ellipse(clientPos.x, clientPos.y, 10)
+}
+
 
 /****** DRAWING CODE  ******/
 
+
+
 function mouseDragged() {
-  
-  
+
+  clientPos.x = mouseX
+  clientPos.y = mouseY
+
+  socket.emit("new circle", clientPos)
 }
 
 function clearCanvas() {
-
+  background("lightblue")
 }
+
+socket.on("new circle", function(newCircle) {
+  clientPos.x = newCircle.x
+  clientPos.y = newCircle.y
+  clientPos.color = newCircle.color
+})
 
